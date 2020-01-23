f603b9fd83eaf4a12722c9fb487913fbf4990632
Эдуард <kholkins@mail.ru>
2020-01-23 21:06:08 +0300

    New

---
+++ src/main/java/com/kholkins/restsimple/RestsimpleApplication.java
@@ -0,0 +1,13 @@
+package com.kholkins.restsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestsimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestsimpleApplication.class, args);
+    }
+
+}
