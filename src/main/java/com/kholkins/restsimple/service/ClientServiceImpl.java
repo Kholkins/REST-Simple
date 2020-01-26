5667475757d57568dbf980817f438d68b88df7bd
Эдуард <kholkins@mail.ru>
2020-01-26 20:18:49 +0300

    New

--- src/main/java/com/kholkins/restsimple/service/ClientServiceImpl.java
+++ src/main/java/com/kholkins/restsimple/service/ClientServiceImpl.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
-
+@Service
 public class ClientServiceImpl implements ClientService {
 
     // Хранилище клиентов
